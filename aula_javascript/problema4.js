e07116a73cf12658ff46b24d3526732ccebe7e13
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2023-08-23 21:04:23 -0300

    first commit

---
+++ aula_javascript/problema4.js
@@ -0,0 +1,7 @@
+var n1 = 10;
+var n2 = 8;
+var n3 = 7;
+
+var media = (n1*2+n2*3+n3*5)/10;
+
+console.log("Media geral é " + media)
---
+++ aula_javascript/problema5.js
@@ -0,0 +1,8 @@
+// Para fazer uma promoção , os comerciantes estão pocurando aumentar as suas vendas oferecendo  desconto.
+//Faça um algortmo que possa receber um valor 'de um produto e que esqueva o novo valor tendo em vista que o desconto é de 9%
+
+var valor = 1000;
+var valordesc = valor*0.09;
+var valorfinal= valor - valordesc;
+
+console.log("o valor final é ",valorfinal,"R$")
